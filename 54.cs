e022640f815f40534a298efc0b261905eb6d203d
Никита Лясковский <124239663+niki6ka@users.noreply.github.com>
2023-03-10 16:05:48 +0300

    Add files via upload

---
+++ 54.cs
@@ -0,0 +1,59 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.WriteLine("Please enter the number of rows in the array : ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter the number of column in the array : ");
+    int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter range : ");
+    int range = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[rows, col];
+CreateArray(array);
+WriteArray(array);
+
+Console.WriteLine("Range array: ");
+OrderArray(array);
+WriteArray(array);
+
+void OrderArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
